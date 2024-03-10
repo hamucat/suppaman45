6f443d0ac6ee8597a3e99ab119f114ad6e99d5c1
Hamu <39907535+hamucat@users.noreply.github.com>
2024-03-10 21:49:32 +0900

    ダイアログにフィルターを付けた。pathがスコープがでかくなってたのでJsonPathに名前変更して分かりやすくした

--- Parzan/MainWindow.xaml.cs
+++ Parzan/MainWindow.xaml.cs
@@ -44,17 +44,20 @@ namespace Parzan
             //suppaman.exeの場所を確認する
             //settings.jsonでないのはない可能性もあって、その場合それを作るのはこっちの仕事なので
             var jsonPathManager = new JsonPathManager();
-            var path = jsonPathManager.LoadPath();
-            if (!File.Exists(path))
+            var jsonPath = jsonPathManager.LoadPath();
+            if (!File.Exists(jsonPath))
             {
                 MessageBox.Show("suppaman45.exeが見つかりませんでした。次に開くダイアログでsuppaman45.exeのある場所を選択してください。","Parzan",MessageBoxButton.OK,MessageBoxImage.Information);
+                
                 var dialog = new OpenFileDialog();
                 dialog.Title = "suppaman45.exeはどこにありますか？";
+                dialog.Filter = "アプリケーション|*.exe";
                 dialog.FileName = "suppaman45.exe";
+                
                 if (dialog.ShowDialog() == true)
                 {
-                    path = System.IO.Path.GetDirectoryName(dialog.FileName) + @"\settings.json";
-                    jsonPathManager.SavePath(path);
+                    jsonPath = System.IO.Path.GetDirectoryName(dialog.FileName) + @"\settings.json";
+                    jsonPathManager.SavePath(jsonPath);
                 }else
                 {
                     this.Close();
@@ -62,10 +65,10 @@ namespace Parzan
                 }
             }
 
-            var settingManager = new suppaman45.SettingManager(path);
+            var settingManager = new suppaman45.SettingManager(jsonPath);
             
             //settings.jsonがなければつくる
-            if (!File.Exists(path))
+            if (!File.Exists(jsonPath))
             {
                 settingManager.SaveSettings(userSettings = new suppaman45.UserSettings());
             }
